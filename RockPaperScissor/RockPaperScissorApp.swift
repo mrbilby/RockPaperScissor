d06a0c9b27e7a1671ef3985ba78235a200b66aa6
mrbilby <baileymrb@gmail.com>
2023-03-22 07:15:22 +0000

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by James Bailey on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
